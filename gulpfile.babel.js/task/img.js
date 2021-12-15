808a532d48df77657e0a58fa149106eb10a7b53d
Ihor Matiev <ihor.matiev@gmail.com>
2021-12-15 13:44:43 +0200

    removed image webp during development

--- gulpfile.babel.js/task/img.js
+++ gulpfile.babel.js/task/img.js
@@ -1,10 +1,14 @@
 export default () => {
+  if ($.app.isProd) {
+    $.gulp
+      .src($.path.img.src)
+      .pipe($.gp.newer($.path.img.dest))
+      .pipe($.gp.webp())
+      .pipe($.gulp.dest($.path.img.dest));
+  }
+
   return $.gulp
     .src($.path.img.src)
-    .pipe($.gp.newer($.path.img.dest))
-    .pipe($.gp.webp())
-    .pipe($.gulp.dest($.path.img.dest))
-    .pipe($.gulp.src($.path.img.src))
     .pipe($.gp.newer($.path.img.dest))
     .pipe($.gp.if($.app.isProd, $.gp.imagemin($.app.imagemin)))
     .pipe($.gulp.dest($.path.img.dest));
